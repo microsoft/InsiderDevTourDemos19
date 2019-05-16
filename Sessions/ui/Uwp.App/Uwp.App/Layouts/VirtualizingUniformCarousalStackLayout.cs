7c54f6f6f8bad90c86dc5a3728367f3bb4a9e3fa
Justin Liu <justin.xin.liu@gmail.com>
2019-05-16 00:10:20 -0700

    Added repeater and other main content on the landing page.

---
+++ Sessions/ui/Uwp.App/Uwp.App/Layouts/VirtualizingUniformCarousalStackLayout.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.UI.Xaml.Controls;
+using System;
+using System.Diagnostics;
+using Windows.Foundation;
+
+namespace Uwp.App.Layouts
+{
+    public class VirtualizingUniformCarousalStackLayout : VirtualizingLayout
+    {
+        public double ItemWidth { get; set; }
+        public double ItemHeight { get; set; }
+        public double Spacing { get; set; }
+
+        protected override Size MeasureOverride(VirtualizingLayoutContext context, Size availableSize)
+        {
+            var realizationRect = context.RealizationRect;
+            int itemCount = context.ItemCount;
+            int firstRealizedIndex = FirstRealizedIndexInRect(realizationRect, itemCount);
+            int lastRealizedIndex = LastRealizedIndexInRect(realizationRect, itemCount);
+            Debug.WriteLine("Measure:" + realizationRect.ToString());
+
+            // Viewport + Buffer Rect.
+            for (int currentIndex = firstRealizedIndex; currentIndex <= lastRealizedIndex; currentIndex++)
+            {
+                var realIndex = Math.Abs(currentIndex % context.ItemCount);
+                var element = context.GetOrCreateElementAt(realIndex);
+                element.Measure(new Size(ItemWidth, ItemHeight));
+            }
+
+            return new Size(ItemWidth * context.ItemCount * 1000, ItemHeight);
+        }
+
+        protected override Size ArrangeOverride(VirtualizingLayoutContext context, Size finalSize)
+        {
+            var realizationRect = context.RealizationRect;
+            int itemCount = context.ItemCount;
+            int firstRealizedIndex = FirstRealizedIndexInRect(realizationRect, itemCount);
+            int lastRealizedIndex = LastRealizedIndexInRect(realizationRect, itemCount);
+            Debug.WriteLine("Arrange:" + realizationRect.ToString());
+
+            // Viewport + Buffer Rect.
+            for (int currentIndex = firstRealizedIndex; currentIndex <= lastRealizedIndex; currentIndex++)
+            {
+                var realIndex = Math.Abs(currentIndex % context.ItemCount);
+                var element = context.GetOrCreateElementAt(realIndex);
+                var arrangeRect = new Rect(currentIndex * (ItemWidth + Spacing), 0, ItemWidth, ItemHeight);
+                element.Arrange(arrangeRect);
+                Debug.WriteLine("   Arrange:" + currentIndex + " :" + arrangeRect);
+            }
+
+            return finalSize;
+        }
+
+        #region Private Helper Methods
+
+        private int FirstRealizedIndexInRect(Rect realizationRect, int itemCount)
+        {
+            return (int)(realizationRect.X / (ItemWidth + Spacing));
+        }
+
+        private int LastRealizedIndexInRect(Rect realizationRect, int itemCount)
+        {
+            int index = (int)(realizationRect.Right / (ItemWidth + Spacing));
+            return index;
+        }
+
+        #endregion Private Methods
+    }
+}
