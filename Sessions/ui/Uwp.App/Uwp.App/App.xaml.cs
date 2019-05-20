bec16a92681188f7b7c600c64a8f63f590837f3b
Justin Liu <justin.xin.liu@gmail.com>
2019-05-20 12:45:41 -0700

    Updated sdk version.

--- Sessions/ui/Uwp.App/Uwp.App/App.xaml.cs
+++ Sessions/ui/Uwp.App/Uwp.App/App.xaml.cs
@@ -68,7 +68,6 @@ namespace Uwp.App
 
                 if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
                 {
-                    //TODO: Load state from previously suspended application
                 }
 
                 // Place the frame in the current Window
@@ -109,7 +108,6 @@ namespace Uwp.App
         private void OnSuspending(object sender, SuspendingEventArgs e)
         {
             var deferral = e.SuspendingOperation.GetDeferral();
-            //TODO: Save application state and stop any background activity
             deferral.Complete();
         }
     }
